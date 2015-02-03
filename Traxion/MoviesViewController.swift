c27d80c53806691d2f0a909d41a5300dc1512a39
Kurt Ruppel <me@kurtruppel.com>
2015-02-02 23:42:09 -0800

    Table view with rows of hello.

---
+++ Traxion/MoviesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MoviesViewController.swift
+//  Traxion
+//
+//  Created by Kurt Ruppel on 2/2/15.
+//  Copyright (c) 2015 kruppel. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        cell.textLabel?.text = "Hello"
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
